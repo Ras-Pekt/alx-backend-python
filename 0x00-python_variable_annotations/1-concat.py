f3912782b4df6b384a7de5087fed3605e42771fe
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2024-01-11 12:01:56 +0300

    basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+def concat(str1: str, str2: str) -> str:
+    """
+    takes a string str1 and a string str2 as arguments and returns a concatenated string
+    """
+    return f"{str1}{str2}"
