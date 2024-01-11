3ccf2be8181ccf43a7df8b85d3379c982ca596c6
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2024-01-11 17:32:17 +0300

    type Checking

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""apply any necessary changes"""
+from typing import List, Tuple
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(int(factor))
+    ]
+    return zoomed_in
+
+
+array = tuple([12, 72, 91])
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3.0)
